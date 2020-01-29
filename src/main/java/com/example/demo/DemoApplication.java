3d2bfb82962f38d1be34e365bea769310b4a17a1
Lugtu <angelo-miguel.lugtu@dxc.com>
2020-01-29 23:43:35 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DemoApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@RequestMapping(value = "/")
+	public String hello() {
+		return "Hello World!";
+	}
+
+}
